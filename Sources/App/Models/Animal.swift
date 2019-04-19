73a93035609bf8f741aa1f057bd4a88e924cee14
Spencer Curtis <spencercolecurtis@gmail.com>
2019-04-19 13:05:50 -0600

    Add memberwise initializer to Animal

--- Sources/App/Models/Animal.swift
+++ Sources/App/Models/Animal.swift
@@ -10,6 +10,16 @@ final class Animal: PostgreSQLModel {
     let longitude: Double
     let description: String
     let imageURL: String
+    
+    init(id: Int?, name: String, timestamp: Date, latitude: Double, longitude: Double, description: String, imageURL: String) {
+        self.id = id
+        self.name = name
+        self.timestamp = timestamp
+        self.latitude = latitude
+        self.longitude = longitude
+        self.description = description
+        self.imageURL = imageURL
+    }
 }
 
 /// Allows `Animal` to be used as a dynamic migration.
