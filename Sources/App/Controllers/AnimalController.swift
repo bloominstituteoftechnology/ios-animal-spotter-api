46b283460a017132e409ccb1a5353fd4cc0f8244
Spencer Curtis <spencercolecurtis@gmail.com>
2019-05-06 00:25:21 -0600

    Add implementations of Codable

--- Sources/App/Controllers/AnimalController.swift
+++ Sources/App/Controllers/AnimalController.swift
@@ -9,7 +9,7 @@ final class AnimalController {
     var animals: [Animal] = [
         Animal(id: 1,
                name: "Lion",
-               timestamp: Date(timeIntervalSince1970: 1476381432),
+               timeSeen: Date(timeIntervalSince1970: 1476381432),
                latitude: 41.0059666,
                longitude: -8.596247,
                description: "A large tawny-colored cat that lives in prides, found in Africa and northwestern India. The male has a flowing shaggy mane and takes little part in hunting, which is done cooperatively by the females.",
@@ -17,7 +17,7 @@ final class AnimalController {
         
         Animal(id: 2,
                name: "Zebra",
-               timestamp: Date(timeIntervalSince1970: 1538180415),
+               timeSeen: Date(timeIntervalSince1970: 1538180415),
                latitude: 45.4401623,
                longitude: 39.5186898,
                description: "An African wild horse with black-and-white stripes and an erect mane.",
@@ -25,7 +25,7 @@ final class AnimalController {
         
         Animal(id: 3,
                name: "Flamingo",
-               timestamp: Date(timeIntervalSince1970: 1483741219),
+               timeSeen: Date(timeIntervalSince1970: 1483741219),
                latitude: 38.7080654,
                longitude: -9.3872558,
                description: "A tall wading bird with mainly pink or scarlet plumage and long legs and neck. It has a heavy bent bill that is held upside down in the water in order to filter-feed on small organisms.",
@@ -33,7 +33,7 @@ final class AnimalController {
         
         Animal(id: 4,
                name: "Brown Meerkat",
-               timestamp: Date(timeIntervalSince1970: 1548905617),
+               timeSeen: Date(timeIntervalSince1970: 1548905617),
                latitude: 31.887402,
                longitude: 115.440283,
                description: "A small southern African mongoose, especially the suricate.",
@@ -41,7 +41,7 @@ final class AnimalController {
         
         Animal(id: 5,
                name: "Baboon",
-               timestamp: Date(timeIntervalSince1970: 1367371199),
+               timeSeen: Date(timeIntervalSince1970: 1367371199),
                latitude: 34.6911,
                longitude: 106.221144,
                description: "A large Old World ground-dwelling monkey with a long doglike snout, large teeth, and naked callosities on the buttocks. Baboons are social animals and live in troops.",
@@ -49,7 +49,7 @@ final class AnimalController {
         
         Animal(id: 6,
                name: "Brown Bear",
-               timestamp: Date(timeIntervalSince1970: 1408047986),
+               timeSeen: Date(timeIntervalSince1970: 1408047986),
                latitude: 56.91683,
                longitude: -117.61945,
                description: "A large bear with a coat color ranging from cream to black, occurring chiefly in forests in Eurasia and North America.",
@@ -57,7 +57,7 @@ final class AnimalController {
         
         Animal(id: 7,
                name: "Hyena",
-               timestamp: Date(timeIntervalSince1970: 1444126635),
+               timeSeen: Date(timeIntervalSince1970: 1444126635),
                latitude: 39.353118,
                longitude: -9.1143068,
                description: "A doglike African mammal with forelimbs that are longer than the hind limbs and an erect mane. Hyenas are noted as scavengers but most are also effective hunters",
@@ -65,7 +65,7 @@ final class AnimalController {
         
         Animal(id: 8,
                name: "Squirrel",
-               timestamp: Date(timeIntervalSince1970: 1529567628),
+               timeSeen: Date(timeIntervalSince1970: 1529567628),
                latitude: 36.6850384,
                longitude: 117.0714935,
                description: "An agile tree-dwelling rodent with a bushy tail, typically feeding on nuts and seeds.",
@@ -73,7 +73,7 @@ final class AnimalController {
         
         Animal(id: 9,
                name: "Python",
-               timestamp: Date(timeIntervalSince1970: 1523244077),
+               timeSeen: Date(timeIntervalSince1970: 1523244077),
                latitude: 49.689425,
                longitude: 21.9126813,
                description: "A large heavy-bodied nonvenomous constrictor snake occurring throughout the Old World tropics.",
@@ -81,7 +81,7 @@ final class AnimalController {
         
         Animal(id: 10,
                name: "Chimpanzee",
-               timestamp: Date(timeIntervalSince1970: 1450987325),
+               timeSeen: Date(timeIntervalSince1970: 1450987325),
                latitude: 12.6417,
                longitude: 123.9092,
                description: "A great ape with large ears, mainly black coloration, and lighter skin on the face, native to the forests of western and central Africa. Chimpanzees show advanced behavior such as the making and using of tools.",
@@ -89,7 +89,7 @@ final class AnimalController {
         
         Animal(id: 11,
                name: "Crab",
-               timestamp: Date(timeIntervalSince1970: 1486234736),
+               timeSeen: Date(timeIntervalSince1970: 1486234736),
                latitude: 11.8859782,
                longitude: -86.054235,
                description: "A crustacean with a broad carapace, stalked eyes, and five pairs of legs, the first pair of which are modified as pincers. Crabs are abundant on many shores, especially in the tropics, where some have become adapted to life on land.",
@@ -97,7 +97,7 @@ final class AnimalController {
         
         Animal(id: 12,
                name: "Jaguar",
-               timestamp: Date(timeIntervalSince1970: 1395166126),
+               timeSeen: Date(timeIntervalSince1970: 1395166126),
                latitude: -10.1429097,
                longitude: 123.6707189,
                description: "A large heavily built cat that has a yellowish-brown coat with black spots, found mainly in the dense forests of Central and South America.",
@@ -105,7 +105,7 @@ final class AnimalController {
         
         Animal(id: 13,
                name: "Eagle",
-               timestamp: Date(timeIntervalSince1970: 1456242326),
+               timeSeen: Date(timeIntervalSince1970: 1456242326),
                latitude: 34.5553494,
                longitude: 69.207486,
                description: "A large bird of prey with a massive hooked bill and long broad wings, renowned for its keen sight and powerful soaring flight.",
@@ -113,7 +113,7 @@ final class AnimalController {
         
         Animal(id: 14,
                name: "Dove",
-               timestamp: Date(timeIntervalSince1970: 1450402567),
+               timeSeen: Date(timeIntervalSince1970: 1450402567),
                latitude: 9.3125556,
                longitude: 42.1226825,
                description: "A stocky seed- or fruit-eating bird with a small head, short legs, and a cooing voice. Doves are generally smaller and more delicate than pigeons, but many kinds have been given both names.",
@@ -121,7 +121,7 @@ final class AnimalController {
         
         Animal(id: 15,
                name: "Dolphin",
-               timestamp: Date(timeIntervalSince1970: 1525001519),
+               timeSeen: Date(timeIntervalSince1970: 1525001519),
                latitude: 6.5592478,
                longitude: 124.6846104,
                description: "A small gregarious toothed whale that typically has a beaklike snout and a curved fin on the back. Dolphins have become well known for their sociable nature and high intelligence.",
@@ -129,7 +129,7 @@ final class AnimalController {
         
         Animal(id: 16,
                name: "Deer",
-               timestamp: Date(timeIntervalSince1970: 1433710478),
+               timeSeen: Date(timeIntervalSince1970: 1433710478),
                latitude: 7.0739678,
                longitude: -73.1692652,
                description: "A hoofed grazing or browsing animal, with branched bony antlers that are shed annually and typically borne only by the male. See also mouse deer, musk deer.",
@@ -137,7 +137,7 @@ final class AnimalController {
         
         Animal(id: 17,
                name: "Tortoise",
-               timestamp: Date(timeIntervalSince1970: 1371092000),
+               timeSeen: Date(timeIntervalSince1970: 1371092000),
                latitude: 53.7002,
                longitude: 102.7117,
                description: "A turtle, typically a herbivorous one that lives on land.",
@@ -145,7 +145,7 @@ final class AnimalController {
         
         Animal(id: 18,
                name: "Seal",
-               timestamp: Date(timeIntervalSince1970: 1497998321),
+               timeSeen: Date(timeIntervalSince1970: 1497998321),
                latitude: 26.655026,
                longitude: 102.244683,
                description: "A fish-eating aquatic mammal with a streamlined body and feet developed as flippers, returning to land to breed or rest.",
@@ -153,7 +153,7 @@ final class AnimalController {
         
         Animal(id: 19,
                name: "Parrot",
-               timestamp: Date(timeIntervalSince1970: 1527470745),
+               timeSeen: Date(timeIntervalSince1970: 1527470745),
                latitude: 10.0016057,
                longitude: 123.4091052,
                description: "A bird, often vividly colored, with a short down-curved hooked bill, grasping feet, and a raucous voice, found especially in the tropics and feeding on fruits and seeds. Many are popular as cage birds, and some are able to mimic the human voice.",
@@ -161,7 +161,7 @@ final class AnimalController {
         
         Animal(id: 20,
                name: "Lizard",
-               timestamp: Date(timeIntervalSince1970: 1375437692),
+               timeSeen: Date(timeIntervalSince1970: 1375437692),
                latitude: 29.995762,
                longitude: 120.586109,
                description: "A reptile that typically has a long body and tail, four legs, movable eyelids, and a rough, scaly, or spiny skin.",
--- Sources/App/Models/Animal.swift
+++ Sources/App/Models/Animal.swift
@@ -2,25 +2,59 @@ import Vapor
 import Authentication
 import FluentSQLite
 
-final class Animal {
+final class Animal: Content {
     let id: Int
     let name: String
-    let timestamp: Date
+    let timeSeen: Date
     let latitude: Double
     let longitude: Double
     let description: String
     let imageURL: String
     
-    init(id: Int, name: String, timestamp: Date, latitude: Double, longitude: Double, description: String, imageURL: String) {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case timeSeen
+        case latitude
+        case longitude
+        case description
+        case imageURL
+    }
+    
+    init(id: Int, name: String, timeSeen: Date, latitude: Double, longitude: Double, description: String, imageURL: String) {
         self.id = id
         self.name = name
-        self.timestamp = timestamp
+        self.timeSeen = timeSeen
         self.latitude = latitude
         self.longitude = longitude
         self.description = description
         self.imageURL = imageURL
     }
+    
+    convenience init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let id = try container.decode(Int.self, forKey: .id)
+        let name = try container.decode(String.self, forKey: .name)
+        let timeSeenInterval = try container.decode(TimeInterval.self, forKey: .timeSeen)
+        let timeSeen = Date(timeIntervalSince1970: timeSeenInterval)
+        let latitude = try container.decode(Double.self, forKey: .latitude)
+        let longitude = try container.decode(Double.self, forKey: .longitude)
+        let description = try container.decode(String.self, forKey: .description)
+        let imageURL = try container.decode(String.self, forKey: .imageURL)
+        
+        self.init(id: id, name: name, timeSeen: timeSeen, latitude: latitude, longitude: longitude, description: description, imageURL: imageURL)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(timeSeen.timeIntervalSince1970, forKey: .timeSeen)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+        try container.encode(description, forKey: .description)
+        try container.encode(imageURL, forKey: .imageURL)
+    }
 }
-
-/// Allows `Animal` to be encoded to and decoded from HTTP messages.
-extension Animal: Content { }
