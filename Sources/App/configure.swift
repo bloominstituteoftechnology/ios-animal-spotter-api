99169ebec95c7bb3bae6e5ec1f717d0d726a2255
Spencer Curtis <spencercolecurtis@gmail.com>
2020-05-07 19:05:59 -0600

    Update to Swift 5.1

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -28,8 +28,8 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Token.self, database: .sqlite)
-    migrations.add(model: User.self, database: .sqlite)
+    migrations.add(model: Token.self, database: DatabaseIdentifier<Token.Database>.sqlite)
+    migrations.add(model: User.self, database: DatabaseIdentifier<User.Database>.sqlite)
     services.register(migrations)
     
     try services.register(AuthenticationProvider())
